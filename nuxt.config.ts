6f4b02dbd13aa1f12a0ba083a20b09a8666315d1
Alejandro Mery <amery@geeks.cl>
2021-11-21 02:21:21 +0000

    enable sass and transpile vuetify
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
     srcDir: 'src/',
 
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+    },
     typescript: {
         strict: true,
     },
